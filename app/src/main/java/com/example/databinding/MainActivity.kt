10207241750fe43e88e4598e028da1c8c618180b
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-01-04 21:08:05 +0900

    DataBinding
    DataBinding

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    var text:String="hello world!"
+    lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.main=this
+
+    }
+    fun clicked(){
+        Toast.makeText(this, "click!", Toast.LENGTH_SHORT).show()}
+}
